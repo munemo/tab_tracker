a9daee80da8f5743b461e5f7f554e7d26ef2b171
munemo <john.chimbani@outlook.com>
2021-12-07 14:23:51 +0100

    fixing nodemon issue

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,18 @@
+//import express from 'express'
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
-app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use(express.json())
 app.use(cors())
 
+app.get('/status', (req,res) => {
 
-app.get('/status',(req,res)=> {
     res.send({
-        message:'hello world!'
+        message: 'hello world'
     })
 })
+
 app.listen(process.env.PORT || 8081)
-
-
